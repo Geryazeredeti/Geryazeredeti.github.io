550105c8d30df3fef8e5cac86002aaef3ee22020
justgery <65452831+Geryazeredeti@users.noreply.github.com>
2022-01-16 13:07:42 +0100

    Add files via upload

---
+++ scripts/cursor.js
@@ -0,0 +1,8 @@
+"use strict";
+        var cursor = document.querySelector("#cursor");
+        var cursorListener = function cursorListener(event){
+            cursor.style.top = event.y + "px";
+            cursor.style.left = event.x + "px";
+        };
+
+        window.addEventListener("mousemove", cursorListener)
